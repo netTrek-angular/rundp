c64baae5a6415e28a2498dcb12907d1e6be62e7b
Saban Ünlü <us@netTrek.de>
2022-02-20 18:16:41 +0100

    chore!: add lib BREAKING CHANGE: ng generate library helper-lib --prefix rp-lib

---
+++ projects/helper-lib/src/lib/helper-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rp-lib-helper-lib',
+  template: `
+    <p>
+      Hello World
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HelperLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
