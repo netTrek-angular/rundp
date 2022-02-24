09231211cf559c550ef3e5e29283785c5e2daa66
Saban Ünlü [netTrek] <us@netTrek.de>
2022-02-24 20:17:51 +0100

    npm install @ngrx/schematics && npx nx g @ngrx/schematics:ng-add npm install @ngrx/store && npx nx g @ngrx/store:ng-add npm install @ngrx/store-devtools && npx nx g @ngrx/store-devtools:ng-add

--- apps/finance/src/app/app.module.ts
+++ apps/finance/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import {ListModule} from "@rp/ui";
 import {AppRoutingModule} from "./app-routing.module";
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, ListModule, AppRoutingModule ],
+  imports: [BrowserModule, ListModule, AppRoutingModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }) ],
   providers: [],
   bootstrap: [AppComponent],
 })
